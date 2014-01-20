e0b7dd8713a52b25d122691cb5a6c10c94516afb
moandji.ezana <mwanji@gmail.com>
2014-01-20 12:43:47 +0200

    LaunchOptions#isBake() checks if any other options were passed in

--- src/main/java/org/jbake/launcher/Main.java
+++ src/main/java/org/jbake/launcher/Main.java
@@ -13,23 +13,23 @@ import org.kohsuke.args4j.CmdLineParser;
 
 /**
  * Launcher for JBake.
- * 
+ *
  * @author Jonathan Bullock <jonbullock@gmail.com>
  *
  */
 public class Main {
 
 	private final String USAGE_PREFIX = "Usage: jbake";
-	
+
 	/**
 	 * Runs the app with the given arguments.
-	 * 
+	 *
 	 * @param String[] args
 	 */
 	public static void main(String[] args) {
 		new Main().run(args);
 	}
-	
+
 	private void bake(LaunchOptions options) {
 		try {
 			Oven oven = new Oven(options.getSource(), options.getDestination());
@@ -51,38 +51,29 @@ public class Main {
 			e.printStackTrace();
 			System.exit(1);
 		}
-		
+
 		System.out.println("JBake " + config.getString("version") + " (" + config.getString("build.timestamp") + ") [http://jbake.org]");
 		System.out.println();
-		boolean didSomething=false;
-		
+
 		if (res.isHelpNeeded()) {
-			didSomething=true;
 			printUsage(res);
 		}
-		
+
 		if(res.isBake()) {
-			didSomething=true;
 			bake(res);
 		}
 
+		if (res.isInit()) {
+		  initStructure(config);
+		}
+
 		if (res.isRunServer()) {
-			didSomething=true;
 			if (res.getSource().getPath().equals(".")) {
 				// use the default destination folder
 				runServer(config.getString("destination.folder"), config.getString("server.port"));
 			} else {
 				runServer(res.getSource().getPath(), config.getString("server.port"));
 			}
-		}
-		
-		if (res.isInit()) {
-			didSomething=true;
-			initStructure(config);
-		}
-		
-		if(!didSomething) {
-			bake(res);
 		}
 	}
 	private LaunchOptions parseArguments(String[] args) {
@@ -93,6 +84,7 @@ public class Main {
 			parser.parseArgument(args);
 		} catch (CmdLineException e) {
 			printUsage(res);
+			System.exit(1);
 		}
 
 		return res;
@@ -106,21 +98,18 @@ public class Main {
 		System.out.println(sw.toString());
 		parser.setUsageWidth(100);
 		parser.printUsage(System.out);
-		System.exit(0);
 	}
 
 	private void runServer(String path, String port) {
 		JettyServer.run(path, port);
-		System.exit(0);
 	}
-	
+
 	private void initStructure(CompositeConfiguration config) {
 		Init init = new Init(config);
 		try {
 			File codeFolder = FileUtil.getRunningLocation();
 			init.run(new File("."), codeFolder);
 			System.out.println("Base folder structure successfully created.");
-			System.exit(0);
 		} catch (Exception e) {
 			System.err.println("Failed to initalise structure!");
 			e.printStackTrace();
